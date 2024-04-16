3b57731714c7cb9d0f4e57baa1bcd51330fbdc3e
Kaushik <jinkal.chaudhari@vernost.in>
2024-04-16 10:23:00 +0530

    java-config-1

--- src/main/java/dev/kc/learnspring/LearnSpringApplication.java
+++ src/main/java/dev/kc/learnspring/LearnSpringApplication.java
@@ -1,14 +1,16 @@
 package dev.kc.learnspring;
 
+import dev.kc.learnspring.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LearnSpringApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(LearnSpringApplication.class, args);
+		ApplicationContext applicationCtx = SpringApplication.run(LearnSpringApplication.class, args);
 	}
 
 }
