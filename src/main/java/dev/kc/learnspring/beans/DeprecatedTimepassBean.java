14116659eb8f89143414f7732bb640071a4cbfdd
Kaushik <jinkal.chaudhari@vernost.in>
2024-06-02 08:40:00 +0530

    remove unwanted files

---
+++ src/main/java/dev/kc/learnspring/beans/DeprecatedTimepassBean.java
@@ -0,0 +1,15 @@
+package dev.kc.learnspring.beans;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Deprecated
+@Primary
+public class DeprecatedTimepassBean implements ITimepassBean{
+
+    @Override
+    public String toString() {
+        return "DeprecatedTimepassBean{}";
+    }
+}
--- src/main/java/dev/kc/learnspring/service/RepoServices.java
+++ src/main/java/dev/kc/learnspring/service/RepoServices.java
@@ -1,5 +1,7 @@
 package dev.kc.learnspring.service;
 
+import dev.kc.learnspring.beans.DeprecatedTimepassBean;
+import dev.kc.learnspring.beans.ITimepassBean;
 import dev.kc.learnspring.repo.IRepo;
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
@@ -8,21 +10,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RepoServices {
 
     private static final Logger log = LoggerFactory.getLogger(RepoServices.class);
+
+    private ITimepassBean timePassBeans;
+
     private IRepo repository;
 
     private IRepo jdbcRepository;
 
     private String appVersion = "NOT_SET";
 
-    public RepoServices() {
-    }
+//    public RepoServices() {
+//        log.info("\nDEFAULT CONSTRUCTOR OF RepoServices invoked");
+//    }
 
 
     public RepoServices(@Value("#{new Integer(environment['app.version'])*3}")String appVersion){
@@ -30,19 +35,22 @@ public class RepoServices {
     }
 
     @Autowired
-    public RepoServices(@Qualifier("jpaRepoImpl") IRepo repository, IRepo jdbcRepoImpl){
+    public RepoServices(@Value("#{new Integer(environment['app.version'])*3}")String appVersion, @Qualifier("jpaRepoImpl") IRepo repository, IRepo jdbcRepoImpl, ITimepassBean timepassBeans){
+        this.appVersion = appVersion;
         this.repository = repository;//This repo is autowired by @Qualifier name
         this.jdbcRepository = jdbcRepoImpl;//This repo is autowired by field name
+        this.timePassBeans = timepassBeans;
     }
 
     @PostConstruct
     public void postInit(){
-        log.info("RepoSerivces postInit");
+        log.info("\nRepoSerivces postInit"+this);
+        log.info("\n IS TIMEPASS BEAN IGNORED DUE TO BFPP ? {}",this.timePassBeans);
     }
 
     @PreDestroy
     public void preDestroy(){
-        log.info("RepoServices preDestroy");
+        log.info("\nRepoServices preDestroy"+this);
     }
 
     public String getAppVersion() {
@@ -50,6 +58,7 @@ public class RepoServices {
     }
 
     public String repoName(){
-      return  String.format("Multiple REPOS %s and %s",this.repository.getRepoName(),this.jdbcRepository.getRepoName());
+        return "hello";
+      //return  String.format("Multiple REPOS %s and %s",this.repository.getRepoName(),this.jdbcRepository.getRepoName());
     }
 }
