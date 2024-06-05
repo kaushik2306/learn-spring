4ab7ead6bb90a59452d4013816e9427824feb394
Kaushik <chaudharikaushik@gmail.com>
2024-06-05 20:14:06 +0530

    commit files

--- src/main/java/dev/kc/learnspring/service/RepoServices.java
+++ src/main/java/dev/kc/learnspring/service/RepoServices.java
@@ -8,30 +8,26 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RepoServices {
 
     private static final Logger log = LoggerFactory.getLogger(RepoServices.class);
-    private IRepo repository;
+
+    private IRepo jpaRepoImplRepository;
 
     private IRepo jdbcRepository;
 
     private String appVersion = "NOT_SET";
-
-    public RepoServices() {
-    }
-
 
     public RepoServices(@Value("#{new Integer(environment['app.version'])*3}")String appVersion){
         this.appVersion = appVersion;
     }
 
     @Autowired
-    public RepoServices(@Qualifier("jpaRepoImpl") IRepo repository, IRepo jdbcRepoImpl){
-        this.repository = repository;//This repo is autowired by @Qualifier name
+    public RepoServices(@Qualifier(value="jpaRepoImpl") IRepo jpaRepoImplRepository, IRepo jdbcRepoImpl){
+        this.jpaRepoImplRepository = jpaRepoImplRepository;//This repo is autowired by @Qualifier name
         this.jdbcRepository = jdbcRepoImpl;//This repo is autowired by field name
     }
 
@@ -50,6 +46,6 @@ public class RepoServices {
     }
 
     public String repoName(){
-      return  String.format("Multiple REPOS %s and %s",this.repository.getRepoName(),this.jdbcRepository.getRepoName());
+      return  String.format("Multiple REPOS %s and %s",this.jpaRepoImplRepository.getRepoName(),this.jdbcRepository.getRepoName());
     }
 }
